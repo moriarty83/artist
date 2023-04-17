1d7b7a3b565e528cc8eca4f7c9dc99eb459968a5
moriarty83 <moriarty83@gmail.com>
2023-04-16 20:03:49 -0400

    bugfix, fonts not showing. Begin styling forms

--- art/forms.py
+++ art/forms.py
@@ -1,14 +1,26 @@
 from django import forms
+
 from .models import Image, Event
 
 
 class ImageForm(forms.ModelForm):
     """Form for the image model"""
     class Meta:
+        forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Name', 'style': 'width: 300px;'}))
         model = Image
         fields = ('title', 'image', 'media', 'description', 'date',
                   'for_sale', 'sold', 'price', 'featured')
-
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'flex flex-row space-x-4 mb-6'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'flex flex-row space-x-4 mb-6'}),
+            'media': forms.TextInput(attrs={'class': 'flex flex-row space-x-4 mb-6'}),
+            'description': forms.Textarea(attrs={'rows': '4', 'cols':'50', 'class': 'flex flex-row space-x-4 mb-6'}),
+            'date': forms.DateInput(attrs={'class': 'flex flex-row space-x-4 mb-6'}),
+            'for_sale': forms.CheckboxInput(attrs={'class': 'flex flex-row space-x-4 mb-6'}),
+            'sold': forms.CheckboxInput(attrs={'class': 'flex flex-row space-x-4 mb-6'}),
+            'price': forms.NumberInput(attrs={'class': 'flex flex-row space-x-4 mb-6'}),
+            'featured': forms.CheckboxInput(attrs={'class': 'flex flex-row space-x-4 mb-6'}),
+        }
 
 class EventForm(forms.ModelForm):
     """Form for the event model"""
